9ae957dfff40e78b39770ebffda710e7820bd077
sbelcic <46656562+sbelcic@users.noreply.github.com>
2026-08-15 20:05:59 +0200

    Add allow-direct-launch argument to Riot Client launch

--- Deceive/StartupHandler.cs
+++ Deceive/StartupHandler.cs
@@ -162,7 +162,7 @@ internal static class StartupHandler
         var startArgs = new ProcessStartInfo { FileName = riotClientPath, Arguments = $"--client-config-url=\"http://127.0.0.1:{proxyServer.ConfigPort}\"" };
 
         if (launchProduct is not null)
-            startArgs.Arguments += $" --launch-product={launchProduct} --launch-patchline={gamePatchline}";
+            startArgs.Arguments += $" --launch-product={launchProduct} --launch-patchline={gamePatchline} --allow-direct-launch";
 
         if (riotClientParams is not null)
             startArgs.Arguments += $" {riotClientParams}";
